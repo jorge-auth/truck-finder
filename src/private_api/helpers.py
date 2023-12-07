eeb66283466dc3de78c38f6165591db1fef899e7
jack <jorge.eduardo.in@gmail.com>
2023-12-07 18:51:49 +0000

    finish the models preparation

---
+++ src/private_api/helpers.py
@@ -0,0 +1,62 @@
+from private_api.models import (
+    Cluster,
+    FoodTruck
+)
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from joblib import dump
+
+def populate_clusters():
+    for i in range(11):
+        c = Cluster(cluster_id=i)
+        c.save()
+
+
+def preprocessing_and_populate_trucks():
+    """
+    /!\ Caution clusters will be attributed with different numbers despite the regions 
+    are the same. Should only be loaded once. The locationid as an unique element guarantees 
+    that.
+    """
+    #extract data from the csv file
+    food_trucks_df = pd.read_csv('dataset/food-truck-data.csv')
+    
+    coordinates = food_trucks_df[['Latitude', 'Longitude']]
+
+    scaler = StandardScaler()
+    coordinates_scaled = scaler.fit_transform(coordinates)
+    kmeans = KMeans(n_clusters=10)
+    food_trucks_df['cluster'] = kmeans.fit_predict(coordinates_scaled)
+    import sys
+    print(sys.path)
+    dump(kmeans, './src/private_api/kmeans_model/kmeans_model.joblib')
+
+    #Load the trucks in the database
+    for index, row in food_trucks_df.iterrows():
+        #get the cluster
+        input_coords_scaled = scaler.transform([[
+            row["Latitude"], 
+            row["Longitude"]]]
+        )
+
+        cluster_id = kmeans.predict(input_coords_scaled)[0]
+        C_i, created = Cluster.objects.get_or_create(
+            cluster_id=cluster_id, 
+            #defaults={'other_field': 'value'}
+        )
+        
+        for index, row in food_trucks_df.iterrows():
+            # Create a FoodTruck instance for each row
+            food_truck_instance = FoodTruck.objects.get_or_create(
+                locationid = row["locationid"],
+                Applicant = row["Applicant"],
+                FacilityType = row["FacilityType"],
+                Address = row["Address"],
+                Latitude = row["Latitude"],
+                Longitude = row["Longitude"],
+                cluster=C_i,  # Link to the Cluster instance
+            )
+        
+        print("Food truck loading complete!")
--- src/private_api/models.py
+++ src/private_api/models.py
@@ -17,21 +17,13 @@ class Cluster(models.Model):
     cluster_id = models.AutoField(primary_key=True)
 
 class FoodTruck(models.Model):
-    locationid = models.AutoField(primary_key=True,unique=True)
-    Applicant = models.CharField(max_length=255)
+    id = models.AutoField(primary_key=True)
+    locationid = models.IntegerField(blank=True, null=True)
+    Applicant = models.CharField(max_length=255,null=True, blank=True)
     FacilityType = models.CharField(max_length=255, null=True, blank=True)
-    cnn = models.IntegerField(null=True, blank=True)
-    #locationDescription = models.TextField(null=True, blank=True)
     Address = models.CharField(max_length=255, null=True, blank=True)
-    blocklot = models.IntegerField(null=True, blank=True)
-    block = models.CharField(max_length=10, null=True, blank=True)
-    lot = models.CharField(max_length=10, null=True, blank=True)
-    permit = models.CharField(max_length=20, null=True, blank=True)
-    Received = models.IntegerField(null=True, blank=True)
-    ExpirationDate = models.CharField(max_length=30, null=True, blank=True)
-    Location = models.CharField(max_length=30, null=True, blank=True)
     Latitude = models.FloatField()
     Longitude = models.FloatField()
-    
-    
+
     cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
+    
